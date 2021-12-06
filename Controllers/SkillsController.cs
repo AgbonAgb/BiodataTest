a7952b90832eea23e1f919cce0f3aee363365d8a
Godwin Agbon <agbonwinn@yahoo.com>
2021-12-06 19:19:59 +0100

    Added Skills

--- Controllers/SkillsController.cs
+++ Controllers/SkillsController.cs
@@ -31,21 +31,72 @@ namespace BiodataTest.Controllers
 
             return View(Sk);
         }
-        public async Task<JsonResult> loadCategories()
+        [HttpPost]
+        public async Task<IActionResult> CreateSkills(Skills Sk)
+        {
+            var skill = await _skills.CreateSkills(Sk);
+
+            if (skill)
+
+            {
+                return RedirectToAction("ExistingSkill");
+            }
+            else
+            {
+                return View(Sk);
+            }
+
+           
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ExistingSkill()
+        {
+            var skill = await _skills.GetSkills();
+
+            return View(skill);
+
+            //if (skill)
+
+            //{
+            //    return RedirectToAction("ExistingSkill");
+            //}
+            //else
+            //{
+                
+            //}
+
+
+        }
+        public async Task<List<Category>> loadCategories()
         {
             //new SelectList(@ViewBag.listofDept,"CategoryID","CategoryName"))
-            var existing = await _category.GetAllCategory();          
+            var existing = await _category.GetAllCategory();
+
+            ////List<Category> list2 = new List<Category>();
+
+            ////list2 = existing.Select(o => new Category
+            ////{
+
+            ////    CategoryName=o.CategoryName,
+            ////    CategoryID = o.CategoryID
+            ////}
+
+            ////).ToList();
+
+            ////list2.Insert(0, new Category { CategoryID = 0, CategoryName = "Select Category" });
+            ///
             List<Category> list2 = existing.AsEnumerable()
                          .Select(o => new Category
                          {
-                             CategoryID= o.CategoryID,
-                             CategoryName=o.CategoryName
-                             
+                             CategoryID = o.CategoryID,
+                             CategoryName = o.CategoryName
+
 
                          }).ToList();
 
 
-            return Json(list2);
+            return (list2);
 
         }
         //public async Task<IActionResult> GetCategories()
--- Models/Skills.cs
+++ Models/Skills.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace BiodataTest.Models
         [Display(Name ="Skill Code")]
         public string skillCode { get; set; }
         [Display(Name = "Skill Description")]
+        [BindProperty, MaxLength(500)]
         public string skillDescription { get; set; }
         public int CategoryID { get; set; }
     }
